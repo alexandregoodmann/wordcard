56ef1ff1eb57167195c63c744cacf2b0c03200ce
alexandregoodmann <alexandregoodmann@gmail.com>
2023-08-10 12:01:27 -0300

    fix proxy

--- src/app/services/word.service.ts
+++ src/app/services/word.service.ts
@@ -1,21 +1,20 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { WordDTO, WordDefinition } from 'src/modal/WordDefinition';
-import { BasicCrudService, httpOptions } from './basic-crud.service';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class WordService extends BasicCrudService<WordDefinition>{
+export class WordService {
 
-  constructor(private http: HttpClient) {
-    super('http://localhost:8082/v1/wordcard', http);
-  }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   search(word: string) {
-    return this.http.get('api/dictionary?q=' + word + '&l=dept', requestOptions);
+    return this.http.get('/api/dictionary?q=' + word + '&l=dept', requestOptions);
   }
 
   add(word: WordDTO): Observable<any> {
@@ -40,9 +39,17 @@ export class WordService extends BasicCrudService<WordDefinition>{
     return list;
   }
 
+  handleError(error) {
+    return throwError(error.error);
+  }
 }
+
 const requestOptions = {
   headers: new HttpHeaders({
     'X-Secret': '1b1138b30031b9b8fb2e8d010d8d4afe0f18a1c51c4795c7f3428ec1a51e651f'
   }),
+};
+
+export const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
